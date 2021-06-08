d8d55e44eaf9f0cd2ec31eda12da77e3679397d5
miily8310s <rillakerika@gmail.com>
2021-06-09 07:17:23 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+import { Player } from "./components";
+</script>
+
+<Player />
---
+++ src/components/Player.svelte
@@ -0,0 +1,9 @@
+<script>
+// your script goes here
+</script>
+
+<h1>Music Player</h1>
+
+<style>
+@import "./Player.scss";
+</style>
