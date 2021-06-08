2e808538414dbde65aac64572ff7258e93759f3b
miily8310s <rillakerika@gmail.com>
2021-06-09 08:16:35 +0900

    Add Play and Pause audio function

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,11 @@
 import { Player } from "./components";
 </script>
 
+<svelte:head>
+  <title>Svelte Music Player</title>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
+  />
+</svelte:head>
 <Player />
--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -1,9 +1,55 @@
-<script>
-// your script goes here
+<script lang="ts">
+let audio: HTMLAudioElement;
+let paused = false;
+
+const playSong = () => {
+  audio.play();
+};
+
+// Pause song
+const pauseSong = () => {
+  audio.pause();
+};
+
+const onClickPlayButton = () => {
+  if (!paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+  paused = !paused;
+};
 </script>
 
 <h1>Music Player</h1>
+<div class="music-container">
+  <div class="music-info">
+    <h4 id="title"></h4>
+    <div class="progress-container" id="progress-container">
+      <div class="progress" id="progress"></div>
+    </div>
+  </div>
+  <audio src="/music/ukulele.mp3" id="audio" bind:this="{audio}"></audio>
+  <div class="img-container">
+    <img src="/images/ukulele.jpg" alt="music-cover" id="cover" />
+  </div>
+  <div class="navigation">
+    <button id="prev" class="action-btn">
+      <i class="fas fa-backward"></i>
+    </button>
+    <button
+      id="play"
+      class="action-btn action-btn-big"
+      on:click="{onClickPlayButton}"
+    >
+      <i class="{paused ? 'fas fa-pause' : 'fas fa-play'}"></i>
+    </button>
+    <button id="next" class="action-btn">
+      <i class="fas fa-forward"></i>
+    </button>
+  </div>
+</div>
 
-<style>
+<style type="text/scss">
 @import "./Player.scss";
 </style>
